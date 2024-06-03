39d37ec1951d442492820160c1917ffd2862ebf1
Adi Wahyudi <adi.whyd02@gmail.com>
2024-06-03 17:07:33 +0700

    feat: add source orders

---
+++ constants/sourceOrder.ts
@@ -0,0 +1,11 @@
+export const sourceOrderType = {
+  WHATSAPP: "Whatsapp",
+  CALL: "Call",
+  EMAIL: "Email",
+};
+
+export const sourceOrderOptions = [
+  { value: sourceOrderType.WHATSAPP, label: "Whatsapp" },
+  { value: sourceOrderType.CALL, label: "Call" },
+  { value: sourceOrderType.EMAIL, label: "Email" },
+];
